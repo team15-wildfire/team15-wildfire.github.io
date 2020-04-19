63b9266f9ecf780dcdf6d652c0ff81c533f16433
team15-wildfire <61565426+team15-wildfire@users.noreply.github.com>
2020-04-19 13:09:52 -0500

    upload for display

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-
-
-// Your web app's Firebase configuration
-  var firebaseConfig = {
+// TODO: Replace with your project's config object. You can find this
+// by navigating to your project's console overview page
+// (https://console.firebase.google.com/project/your-project-id/overview)
+// and clicking "Add Firebase to your web app"
+var firebaseConfig = {
     apiKey: "AIzaSyCSf68r57bQOzB2NyRCQg6pm2fPjgRdj_U",
     authDomain: "team15-wildfire-test.firebaseapp.com",
     databaseURL: "https://team15-wildfire-test.firebaseio.com",
@@ -13,11 +14,12 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-var dataset = firebase.database().ref("dataset");
+// Reference to the datasets object in your Firebase database
+var datasets = firebase.database().ref("datasets");
 
-// write new dataset to database
 
- var submitDataset = function () {
+// Save a new dataset to the database, using the input in the form
+var submitdataset = function () {
 
   // Get input values from each of the form elements
   var nodepos = $("#talknodepos").val();
@@ -26,19 +28,21 @@ var dataset = firebase.database().ref("dataset");
   var co = $("#talkco").val();
 
   // Push a new dataset to the database using those values
-  dataset.push({
+  datasets.push({
     "nodepos": nodepos,
     "time": time,
     "temp": temp,
-    "co": co
+	"co": co
   });
 };
+
+
 
 // Get the single most recent dataset from the database and
 // update the table with its values. This is called every time the child_added
 // event is triggered on the datasets Firebase reference, which means
 // that this will update EVEN IF you don't refresh the page. Magic.
-dataset.limitToLast(1).on('child_added', function(childSnapshot) {
+datasets.limitToLast(1).on('child_added', function(childSnapshot) {
   // Get the dataset data from the most recent snapshot of data
   // added to the datasets list in Firebase
   dataset = childSnapshot.val();
@@ -48,19 +52,18 @@ dataset.limitToLast(1).on('child_added', function(childSnapshot) {
   $("#time").html(dataset.time)
   $("#temp").html(dataset.temp)
   $("#co").html(dataset.co)
-
   
 });
 
 // When the window is fully loaded, call this function.
-// Note: because I am attaching an event listener to a particular HTML element
-// in this function, I can't do that until the HTML element in question has
-// been loaded. Otherwise, I'm attaching our listener to nothing, and no code
+// Note: because we are attaching an event listener to a particular HTML element
+// in this function, we can't do that until the HTML element in question has
+// been loaded. Otherwise, we're attaching our listener to nothing, and no code
 // will run when the submit button is clicked.
 $(window).load(function () {
 
   // Find the HTML element with the id datasetForm, and when the submit
-  // event is triggered on that element, call submitDataset.
-  $("#datasetForm").submit(submitDataset);
+  // event is triggered on that element, call submitdataset.
+  $("#datasetForm").submit(submitdataset);
 
 });
